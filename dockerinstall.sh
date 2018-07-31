401282756ceda82ea7c94a22bf12aff7e2ee2ed8
goutham639 <goutham639@gmail.com>
2018-07-31 14:14:48 +0000

    changes to dockerinstallation file

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,9 +1,14 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#update the packages
 sudo apt-get update
 apt-cache policy docker-ce
+#install the docker 
 sudo apt-get install -y docker-ce
+#status of the docker
 sudo systemctl status docker
+#add a user named docker
 sudo usermod -aG docker ${USER}
 su - ${USER}
+#command to check whether the docker is installed or not
 docker info
